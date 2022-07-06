8d0131ac90c50596b222d0cfff68e07c884d110e
jmlinger <jmlinger10@gmail.com>
2022-07-06 20:27:35 -0300

    feat: implementa link aos cards dos restaurantes.

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,36 +1,40 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 function RestaurantCard(props) {
-  const { restaurant: { name, subname, image, rating, favUsers } } = props;
+  const { restaurant: { id, name, subname, image, rating, favUsers } } = props;
 
   return (
-    <div>
-      <img alt="banner do restaurante" src={ image } />
+    <Link to={ `/restaurants/${id}` }>
       <div>
-        <h1>{name}</h1>
-        <h2>{subname}</h2>
-        <p>
-          <img alt="estrela-avaliação" src="" />
-          {rating}
-        </p>
-        <p htmlFor="imagem-relogio">
-          <img alt="relogio-tempo-deslocamento" src="" />
-          30-40 min
-        </p>
-        <img
-          alt="favoritado"
-          src=""
-          hidden={ !favUsers }
-        />
+        <img alt="banner do restaurante" src={ image } />
+        <div>
+          <h1>{name}</h1>
+          <h2>{subname}</h2>
+          <p>
+            <img alt="estrela-avaliação" src="" />
+            {rating}
+          </p>
+          <p htmlFor="imagem-relogio">
+            <img alt="relogio-tempo-deslocamento" src="" />
+            30-40 min
+          </p>
+          <img
+            alt="favoritado"
+            src=""
+            hidden={ !favUsers }
+          />
+        </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
 export default RestaurantCard;
 
 RestaurantCard.propTypes = ({
+  key: PropTypes.number,
   restaurant: PropTypes.object,
   name: PropTypes.string,
   image: PropTypes.string,
