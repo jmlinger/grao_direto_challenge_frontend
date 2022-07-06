bfa572cdb0ea00d8ea40022b7a76fc44a6138a3a
jmlinger <jmlinger10@gmail.com>
2022-07-05 23:08:00 -0300

    feat: implemeta rotas: registro e restaurantes.

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 import Login from '../pages/Login';
-// import Register from '../pages/Register';
-// import Restaurants from '../pages/Restaurants';
+import Register from '../pages/Register';
+import Restaurants from '../pages/Restaurants';
 // import RestaurantsDetail from '../pages/RestaurantsDetail';
 // import Profile from '../pages/Profile';
 // import Error from '../pages/Error';
@@ -11,10 +11,10 @@ function allRoutes() {
   return (
     <Routes>
       <Route path="/" element={ <Login /> } />
-      {/* <Route path="/register" element={ <Register /> } />
+      <Route path="/register" element={ <Register /> } />
       <Route path="/restaurants" element={ <Restaurants /> } />
-      <Route path="/restaurants/:id" element={ <RestaurantsDetail /> } />
-      <Route path="/profile" element={ <Profile /> } /> */}
+      {/* <Route path="/restaurants/:id" element={ <RestaurantsDetail /> } /> */}
+      {/* <Route path="/profile" element={ <Profile /> } /> */}
       {/* <Route path="*" element={ <Error /> } /> */}
     </Routes>
   );
