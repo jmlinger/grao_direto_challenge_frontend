7ba512ab73a2ca8992c2fa5708cdf7c562e408bd
jmlinger <jmlinger10@gmail.com>
2022-07-07 16:50:51 -0300

    feat: implementa css para as páginas Login, Register e RestaurantDetails e para o componente Footer .

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,36 +1,32 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Link, useLocation } from 'react-router-dom';
+import { FooterStyle } from '../styles/Footer';
 
 function Footer(props) {
   const { setSearchBarVisibility } = props;
   const { pathname } = useLocation();
 
   return (
-    <footer>
+    <FooterStyle>
       <Link to="/restaurants">
         <img alt="página-principal" src="" />
       </Link>
       <button
         type="button"
-        hidden={ pathname !== '/restaurants' }
-        onClick={ () => setSearchBarVisibility() }
-      >
-        <img
-          alt="barra de busca"
-          src=""
-
-        />
+        hidden={pathname !== '/restaurants'}
+        onClick={() => setSearchBarVisibility()}>
+        <img alt="barra de busca" src="" />
       </button>
       <Link to="/profile">
         <img alt="perfil" src="" />
       </Link>
-    </footer>
+    </FooterStyle>
   );
 }
 
 export default Footer;
 
-Footer.propTypes = ({
-  setSearchBarVisibility: PropTypes.func,
-}).isRequired;
+Footer.propTypes = {
+  setSearchBarVisibility: PropTypes.func
+}.isRequired;
