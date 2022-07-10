97761cd4b7fb0075de449c75709de1be5483763e
jmlinger <jmlinger10@gmail.com>
2022-07-10 19:07:24 -0300

    feat: implementa estilo ao carrossel.

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,46 +1,30 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import Slider from 'react-slick';
-import '../styles/RestaurantDetails.css';
+import '../styles/components/Carosel.css';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const Carousel = (props) => {
   const { menu } = props;
-  console.log(menu);
 
   const settings = {
-    className: 'Carousel',
-    dots: false,
+    dots: true,
     infinite: true,
     autoplay: true,
     autoplaySpeed: 4000,
     speed: 500,
     pauseOnFocus: true,
-    slidesToShow: 4,
-    slidesToScroll: 4,
+    slidesToShow: 3,
+    slidesToScroll: 1,
     initialSlide: 0,
     responsive: [
       {
-        breakpoint: 1024,
+        breakpoint: 800,
         settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
+          arrows: false,
+          dots: false,
           infinite: true,
-          dots: true
-        }
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2
-        }
-      },
-      {
-        breakpoint: 480,
-        settings: {
           slidesToShow: 2,
           slidesToScroll: 1
         }
@@ -59,7 +43,7 @@ const Carousel = (props) => {
             </div>
             <div className="card-bottom">
               <p>{description}</p>
-              <p className="price">{price}</p>
+              <p className="price">R$ {price.replace('.', ',')}</p>
             </div>
           </div>
         ))}
