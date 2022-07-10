8b1337c8640d7d5419e17ef1864f08b8bcb79755
jmlinger <jmlinger10@gmail.com>
2022-07-10 18:53:21 -0300

    feat: seta icone coraçao cheio quando já favoritado e implementa filtro para meals e drinks no carrossel.

--- src/pages/RestaurantDetails.jsx
+++ src/pages/RestaurantDetails.jsx
@@ -1,11 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { MdFavoriteBorder, MdFavorite } from 'react-icons/md';
-import { AiFillStar, AiOutlineFieldTime } from 'react-icons/ai';
 import Footer from '../components/Footer';
 import { apiFavRes, apiGetRestaurantById, apiUnFavRes } from '../services/apiCalls';
-import '../styles/RestaurantDetails.css';
 import Carousel from '../components/Carousel';
+import RestaurantCard from '../components/RestaurantCard';
+import { MainDiv, MenuSection } from '../styles/pages/RestaurantDetails';
 
 function RestaurantDatails() {
   const { id: restaurantId } = useParams();
@@ -17,74 +16,38 @@ function RestaurantDatails() {
     const get = async () => {
       const response = await apiGetRestaurantById(restaurantId);
       if (!response.error) {
-        setRestaurant(response.restaurant);
+        const { restaurant } = response;
+        setRestaurant(restaurant);
+        restaurant.favUsers && setFavOn(true);
       }
     };
     get();
   }, []);
 
-  const handleClick = async () => {
+  const { menu } = restaurant;
+
+  const favResByClick = async () => {
     setFavOn(!favOn);
     // eslint-disable-next-line no-unused-expressions
     favOn ? await apiUnFavRes(restaurantId) : await apiFavRes(Number(restaurantId));
   };
 
-  const { name, subname, image, phone, address, rating } = restaurant;
+  const meals = menu && menu.filter(({ category }) => category === 'refeição');
+  const drinks = menu && menu.filter(({ category }) => category === 'bebida');
 
   return (
     <div>
-      <div className="main-div">
-        <header>
-          <img className="res-img" alt="banner do restaurante" src={image} />
-          <div className="res-info-container">
-            <div className="main-info-container">
-              <div className="icons-container">
-                <p>
-                  <AiFillStar />
-                  {rating}
-                </p>
-                <p htmlFor="imagem-relogio">
-                  <AiOutlineFieldTime />
-                  30-40 min
-                </p>
-              </div>
-              <div className="names-container">
-                <h1>{name}</h1>
-                <h3>{subname}</h3>
-              </div>
-              <div className="icons-container">
-                <button className="fav-button" type="button" onClick={() => handleClick()}>
-                  {!favOn ? (
-                    <MdFavoriteBorder className="fav-icons" />
-                  ) : (
-                    <MdFavorite className="fav-icons" />
-                  )}
-                </button>
-              </div>
-            </div>
-            <div className="contact-info-container">
-              <p>{phone && phone.replace(/(\d{1})(\d{2})(\d{5})(\d{3})/, '($2) $3-$4')}</p>
-              <p>{address}</p>
-            </div>
-          </div>
-        </header>
-        <section className="menu-section">
+      <MainDiv>
+        <RestaurantCard restaurant={restaurant} favResByClick={favResByClick} favOn={favOn} />
+        <MenuSection>
           <h3>Refeições</h3>
-          {Object.keys(restaurant).length > 0 && <Carousel menu={restaurant.menu} />}
-        </section>
-        <section className="menu-section">
+          {Object.keys(restaurant).length > 0 && <Carousel menu={meals} />}
+        </MenuSection>
+        <MenuSection>
           <h3>Bebidas</h3>
-          {Object.keys(restaurant).length > 0 && <Carousel menu={restaurant.menu} />}
-        </section>
-        <section className="menu-section">
-          <h3>Bebidas</h3>
-          {Object.keys(restaurant).length > 0 && <Carousel menu={restaurant.menu} />}
-        </section>
-        <section className="menu-section">
-          <h3>Bebidas</h3>
-          {Object.keys(restaurant).length > 0 && <Carousel menu={restaurant.menu} />}
-        </section>
-      </div>
+          {Object.keys(restaurant).length > 0 && <Carousel menu={drinks} />}
+        </MenuSection>
+      </MainDiv>
       <Footer />
     </div>
   );
