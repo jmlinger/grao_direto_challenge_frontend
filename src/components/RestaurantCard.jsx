143976bb9d2dfea7f3b2e17840ba536c064a497f
jmlinger <jmlinger10@gmail.com>
2022-07-10 18:56:15 -0300

    feat: implementa componente restaurantCard.

--- src/components/RestaurantCard.jsx
+++ src/components/RestaurantCard.jsx
@@ -1,31 +1,64 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+import {
+  Card,
+  ContactInfo,
+  IconsContainer,
+  MainInfo,
+  NamesContainer,
+  RestInfo
+} from '../styles/components/RestaurantCard';
+import { FavOffIcon, FavOnIcon, RatingStarIcon, TimeIcon } from '../styles/components/Icons';
 
 function RestaurantCard(props) {
+  const { pathname } = useLocation();
   const {
-    restaurant: { id, name, subname, image, rating, favUsers }
+    restaurant: { name, subname, image, rating, phone, address, favUsers },
+    favResByClick,
+    favOn
   } = props;
 
+  const mainPage = pathname === '/restaurants';
+
+  const favButtom = () => {
+    if (mainPage) {
+      return <button type="button">{!favUsers ? <FavOffIcon /> : <FavOnIcon />}</button>;
+    }
+    return (
+      <button type="button" onClick={() => favResByClick()}>
+        {!favOn ? <FavOffIcon /> : <FavOnIcon />}
+      </button>
+    );
+  };
+
   return (
-    <Link to={`/restaurants/${id}`}>
-      <div>
-        <img alt="banner do restaurante" src={image} />
-        <div>
-          <h1>{name}</h1>
-          <h2>{subname}</h2>
-          <p>
-            <img alt="estrela-avaliação" src="" />
-            {rating}
-          </p>
-          <p htmlFor="imagem-relogio">
-            <img alt="relogio-tempo-deslocamento" src="" />
-            30-40 min
-          </p>
-          <img alt="favoritado" src="" hidden={!favUsers} />
-        </div>
-      </div>
-    </Link>
+    <Card pathname={pathname}>
+      <img alt="banner do restaurante" src={image} />
+      <RestInfo>
+        <MainInfo>
+          <IconsContainer>
+            <div>
+              <RatingStarIcon />
+              <p>{rating}</p>
+            </div>
+            <div>
+              <TimeIcon />
+              <p>30-40 min</p>
+            </div>
+          </IconsContainer>
+          <NamesContainer pathname={pathname}>
+            <h1>{name}</h1>
+            <h3>{subname}</h3>
+          </NamesContainer>
+          <IconsContainer>{favButtom()}</IconsContainer>
+        </MainInfo>
+        <ContactInfo hidden={mainPage}>
+          <p>{phone && phone.replace(/(\d{1})(\d{2})(\d{5})(\d{3})/, '($2) $3-$4')}</p>
+          <p>{address}</p>
+        </ContactInfo>
+      </RestInfo>
+    </Card>
   );
 }
 
@@ -36,5 +69,10 @@ RestaurantCard.propTypes = {
   restaurant: PropTypes.object,
   name: PropTypes.string,
   image: PropTypes.string,
-  rating: PropTypes.number
+  rating: PropTypes.number,
+  address: PropTypes.string,
+  phone: PropTypes.string,
+  favUsers: PropTypes.string,
+  favResByClick: PropTypes.func,
+  favOn: PropTypes.bool
 }.isRequired;
